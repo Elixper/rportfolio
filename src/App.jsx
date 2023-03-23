f60205645b9883435ae3494652001f02fb44cdaa
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-03-23 19:33:27 +0100

    worked on mainpage css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faMoon} from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin,faGithub} from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin,faGithub} from "@fortawesome/free-brands-svg-icons";
 import { useState } from 'react';
+import photo from "../src/assets/photo.png";
 import './App.css';
 
 function App() {
@@ -18,19 +19,20 @@ function App() {
             className="cursor-pointer text-2xl"
             onClick={() => setDarkMode(!darkMode)}/></li>
             <li><a className=" bg-gradient-to-r from-menuPurple to-blue-400 text-white px-4 py-2 rounded ml-8" href="#">About</a></li>
-            <li><a className=" bg-gradient-to-r from-menuPurple to-blue-400 text-white px-4 py-2 rounded ml-8" href="#">Clients</a></li>
-            <li><a className=" bg-gradient-to-r from-menuPurple to-blue-400 text-white px-4 py-2 rounded ml-8" href="#">Contact</a></li>
+            {/* <li><a className=" bg-gradient-to-r from-menuPurple to-blue-400 text-white px-4 py-2 rounded ml-8" href="#">Clients</a></li>
+            <li><a className=" bg-gradient-to-r from-menuPurple to-blue-400 text-white px-4 py-2 rounded ml-8" href="#">Contact</a></li> */}
           </ul>
         </nav>
-        <div>
-          <h2>Ludwig PEREIRA</h2>
-          <h3>Freelance Frontend Developer</h3>
-          <p>1,5 year creative Frontend Developer, with a gamification design background. 8+ years in business exports helped me develop strong interpersonal skills and collaborate with peer developers with an emphasis on feedback and sharing best practices.</p>
+        <div className="text-center p-10">
+          <h2 className="text-5xl py-2 text-menuPurple font-medium">Ludwig PEREIRA</h2>
+          <h3 className="text-2xl py-2">Freelance Frontend Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-600">1,5 year creative Frontend Developer, with a gamification design background. 8+ years in business exports helped me develop strong interpersonal skills and collaborate with peer developers with an emphasis on feedback and sharing best practices.</p>
         </div>
-        <div>
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
         <FontAwesomeIcon icon={faLinkedin}/>
         <FontAwesomeIcon icon={faGithub}/>
         </div>
+        <div><img src={photo} alt="profilePicture"/></div>
       </section>
     </main>
       
